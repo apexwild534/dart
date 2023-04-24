23d96a3131dc989380033b545ca2ea188e3d03c7
apexwild534 <kevin15dhankhar2004@gmail.com>
2023-04-24 21:07:27 +0530

    first commit

---
+++ 15.dart
@@ -0,0 +1,12 @@
+// 1. Required Parameters
+void main() {
+  printCities("New York", "New Delhi", "Sydney");
+  print("");
+}
+
+// Required Parameters
+void printCities(String name1, String name2, String name3) {
+  print("Name 1 is $name1");
+  print("Name 2 is $name2");
+  print("Name 3 is $name3");
+}
---
+++ 2.dart
@@ -0,0 +1,33 @@
+void main(List<String> arguments) {
+  // Numbers: int
+  int score = 23;
+  var count = 23; // It is inferred as integer automatically by compiler
+  int hexValue = 0xEADEBAEE;
+
+  // Numbers: double
+  double percentage = 93.4;
+  var percent = 82.533;
+  double exponents = 1.42e5;
+
+  // Strings
+  String name = "Henry";
+  var company = "Google";
+
+  // Boolean
+  bool isValid = true;
+  var isAlive = false;
+
+  print(score);
+  print(exponents);
+  print(count);
+  print(hexValue);
+  print(percent);
+  print(percentage);
+  print(name);
+  print(company);
+  print(isAlive);
+  print(isValid);
+
+  // NOTE: All data types in Dart are Objects.
+  // Therefore, their initial value is by default 'null'
+}
